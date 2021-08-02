f31474d23fb322a61e458fb960b36e047227e51b
zbuchholz <46008954+zbuchholz@users.noreply.github.com>
2021-08-02 15:15:12 -0500

    Update mapscript.js

--- mapscript.js
+++ mapscript.js
@@ -258,11 +258,30 @@ function initializeSearchInput(locator) {
     geocodeSearch(searchInputEl.value.trim());
   });
 
-  // Add in an event listener for the Enter key.
-  searchInputEl.addEventListener('keypress', function(evt) {
-    if (evt.key === 'Enter') {
-      geocodeSearch(searchInputEl.value);
+  // Initialize Autocomplete.
+  initializeSearchInputAutocomplete(
+      locator, searchInputEl, geocodeSearch, updateSearchLocation);
+}
+
+/** Add Autocomplete to the search input. */
+function initializeSearchInputAutocomplete(
+    locator, searchInputEl, fallbackSearch, searchLocationUpdater) {
+  // Set up Autocomplete on the search input. Bias results to map viewport.
+  const autocomplete = new google.maps.places.Autocomplete(searchInputEl, {
+    types: ['geocode'],
+    fields: ['place_id', 'formatted_address', 'geometry.location']
+  });
+  autocomplete.bindTo('bounds', locator.map);
+  autocomplete.addListener('place_changed', function() {
+    const placeResult = autocomplete.getPlace();
+    if (!placeResult.geometry) {
+      // Hitting 'Enter' without selecting a suggestion will result in a
+      // placeResult with only the text input value as the 'name' field.
+      fallbackSearch(placeResult.name);
+      return;
     }
+    searchLocationUpdater(
+        placeResult.formatted_address, placeResult.geometry.location);
   });
 }
 
